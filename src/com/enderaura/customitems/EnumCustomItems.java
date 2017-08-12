e3b33b1a289a15b631850c24fa0f6abb38775d4d
Matthew Haskett <matthewhaskett@live.co.uk>
2017-08-12 10:19:53 +0100

    Added an enum

---
+++ src/com/enderaura/customitems/EnumCustomItems.java
@@ -0,0 +1,20 @@
+package com.enderaura.customitems;
+
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * Created by Enderqura on 12/08/2017 at 10:18.
+ */
+public enum EnumCustomItems {
+
+    BUTTERFLY_KNIFE,
+    ZOMBIE_KNIFE;
+
+    private
+
+    EnumCustomItems(ItemStack item){
+
+
+    }
+
+}
